fbd65cfd0bc7cbe5813429cba966acbe0a8af5b9
Cedric Nagata <cedric.nagata@gideontaylor.com>
2025-09-05 09:25:29 -0700

    Initial Commit

---
+++ little-chef/little_chefApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_chefApp.swift
+//  little-chef
+//
+//  Created by Cedric Nagata on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct little_chefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ little-chefTests/little_chefTests.swift
@@ -0,0 +1,17 @@
+//
+//  little_chefTests.swift
+//  little-chefTests
+//
+//  Created by Cedric Nagata on 9/5/25.
+//
+
+import Testing
+@testable import little_chef
+
+struct little_chefTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
